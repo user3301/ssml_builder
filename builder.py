4be5ffefecb801d954245a48404ca8a551eb3b86
user3301 <stan_gai@Hotmail.com>
2018-02-05 14:01:16 +1100

    :heavy_plus_sign:paragraph func

--- builder.py
+++ builder.py
@@ -14,7 +14,15 @@ class Speech:
         self.content += self.escape(saying)
         return self
 
+    # inserts a paragraph tag.
+    # @param paragraph The paragraph of text to insert
+    # @returns {self}
+    def paragraph(self,paragraph):
+        self.present(paragraph,"The paragraph was null")
+        self.content += "<p>" + self.escape(paragraph) + "</p>"
+        return self
 
+        
     # Validates that the provided value is not null or undefined. It will throw an exception if it's either.
     def present(self,value,msg):
         if value is None:
@@ -24,16 +32,13 @@ class Speech:
     # This method escapes any special characters that will cause SSML to be invalid
     def escape(self,word):
         if isinstance(word,str):
-
-            return word
-        if isinstance(word,(int,float,complex)):
             word = word.replace('&' 'and')
             word = word.replace('<', '')
             word = word.replace('>', '')
             word = word.replace('"', '')
             word = word.replace('\'', '')
             return word
-        if isinstance(word,bool):
+        if isinstance(word,(int,float,complex,bool)):
             return word
         raise Exception("received invalid type")
 
