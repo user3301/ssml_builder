f488d42d555866710687eafcf357a57b7c128a77
user3301 <stan_gai@Hotmail.com>
2018-02-05 12:55:06 +1100

    :construction:WIP

--- builder.py
+++ builder.py
@@ -1,13 +1,17 @@
+import string,numbers,types
+NumberTypes = (types.IntType,types.LongType,types.FloatType,types.ComplexType)
+
 class Speech:
 
     # @constructor
     def __init__(self):
-        self.content = []
+        self.content = ""
 
     # This appends raw text into the <speak/> tag
     def say(self,saying):
         self.present(saying,"The saying provided was null")
-        self.content +=
+        self.content += self.escape(saying)
+        return self
 
 
     # Validates that the provided value is not null or undefined. It will throw an exception if it's either.
@@ -17,4 +21,13 @@ class Speech:
 
 
     # This method escapes any special characters that will cause SSML to be invalid
+    def escape(self,word):
+        if isinstance(word,(str,unicode)):
+            return word
+        if isinstance(word,NumberTypes):
+            return word
+        if isinstance(word,bool):
+            return word
+        raise Exception("received invalid type")
 
+    
