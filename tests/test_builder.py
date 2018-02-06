b20fe418b431bb9e39b56da146798413fc22115c
user3301 <stan_gai@Hotmail.com>
2018-02-06 12:55:33 +1100

    :white_check_mark:test case for say & ssml function

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -6,11 +6,12 @@ class Test_Builder(unittest.TestCase):
 
     def test_010_Say(self):
         speech = Speech()
-        speech.say("Hello,this is polly")
+        speech.say("Hello,this is polly.")
+        speech.say("Good to see you.")
 
-        self.assertEqual(speech.content, ['Hello,this is polly'])
-        self.assertEqual(speech.ssml(True), "Hello,this is polly")
-        self.assertEqual(speech.ssml(False), "<speak>Hello,this is polly</speak>")
+        self.assertEqual(speech.content, ['Hello,this is polly.', 'Good to see you.'])
+        self.assertEqual(speech.ssml(True), "Hello,this is polly. Good to see you.")
+        self.assertEqual(speech.ssml(False), "<speak>Hello,this is polly. Good to see you.</speak>")
 
 
 if __name__ == "__main__":
